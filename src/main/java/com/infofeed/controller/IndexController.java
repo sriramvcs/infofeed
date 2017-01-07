fd469d70f95883174c298332ad8d3b56815235b3
sriramvcs <sriramvcs@gmail.com>
2017-01-07 13:23:14 -0500

    Initial commit

---
+++ src/main/java/com/infofeed/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.infofeed.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by sriramvcs on 2017-01-07.
+ */
+@RestController
+public class IndexController {
+
+    @RequestMapping("/")
+    public String getIndex() {
+        return "Hello World!";
+    }
+}
